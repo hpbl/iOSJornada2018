0591f2acd854054e71f89d4ecee224af264f45b2
Hilton Pintor <hiltonpintor@gmail.com>
2018-07-27 15:12:40 -0300

    Adicionando tab bar de veiculos

---
+++ Aulas/Aula 06/Tab Bar Grande/Tab Bar Grande/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tab Bar Grande
+//
+//  Created by Hilton Pintor Bezerra Leite on 27/07/18.
+//  Copyright © 2018 Hilton Pintor Bezerra Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
